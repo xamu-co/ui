1c7779c0d83c24ea3b65a5d9340d8ffb2fcbdf91
Victor Saa <vis97c@hotmail.com>
2024-05-24 13:10:08 -0500

    fix: extend alert support on action

--- packages/components-vue/src/components/action/Button.stories.ts
+++ packages/components-vue/src/components/action/Button.stories.ts
@@ -1,17 +1,30 @@
 import type { Meta, StoryObj } from "@storybook/vue3";
 
-import Button from "./Button.vue";
+import ButtonComponent from "./Button.vue";
+import { eColors } from "@open-xamu-co/ui-common-enums";
 
 const meta = {
 	title: "Action/Action Button",
-	component: Button as Record<keyof typeof Button, unknown>,
+	component: ButtonComponent as Record<keyof typeof ButtonComponent, unknown>,
 	args: { default: "Action Button" },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof ButtonComponent>;
 
-type Story = StoryObj<typeof Button>;
+type Story = StoryObj<typeof ButtonComponent>;
 
-export const Sample: Story = {
+export const Button: Story = {
 	args: { default: "Action Button" },
+};
+
+export const ButtonIsActiveWithAlert: Story = {
+	args: { default: "Button with alert", active: true, alert: true },
+};
+
+export const ButtonAsY: Story = {
+	args: { default: "⋮", y: true, theme: eColors.DANGER },
+};
+
+export const ButtonAsYIsActiveWithAlert: Story = {
+	args: { default: "⋮", y: true, theme: eColors.DANGER, active: true, alert: true },
 };
 
 export default meta;
