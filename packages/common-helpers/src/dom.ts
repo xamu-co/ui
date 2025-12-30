69ab14c7b32c3fa3ca426f378db940d5a0fe48c6
Victor Saa <vis97c@hotmail.com>
2025-12-29 20:41:17 -0500

    refactor: prefer modern syntax

--- packages/common-helpers/src/dom.ts
+++ packages/common-helpers/src/dom.ts
@@ -47,10 +47,9 @@ export function scrollTo(section: string) {
  * Copy the given string to the user clipboard
  * This uses the more modern clipboard api, but using the previos function as a fallback
  *
- * @export
  * @param {string} str a normal string
  */
-export function srtToClipboard(str: string) {
+export async function srtToClipboard(str: string): Promise<boolean> {
 	/** Fallback - copy the given string to the user clipboard */
 	function oldStrToClipboard(str: string) {
 		const el = document.createElement("textarea");
@@ -62,60 +61,56 @@ export function srtToClipboard(str: string) {
 		document.body.removeChild(el);
 	}
 
-	return new Promise<boolean>((resolve) => {
-		if ("clipboard" in navigator) {
-			navigator.clipboard
-				.writeText(str)
-				.then(() => {
-					// Copiado moderno
-					resolve(true);
-				})
-				.catch((error) => {
-					// This can happen if the user denies clipboard permissions:
-					// eslint-disable-next-line no-console
-					console.log("Could not copy text", error);
-					// reject(new Error("Could not copy text"));
-					resolve(false);
-				});
-		} else if (oldStrToClipboard(str) !== null) {
-			// Copiado con fallback
-			resolve(true);
+	if ("clipboard" in navigator) {
+		try {
+			await navigator.clipboard.writeText(str);
+
+			// Copiado moderno
+			return true;
+		} catch (err) {
+			// This can happen if the user denies clipboard permissions:
+			// eslint-disable-next-line no-console
+			console.log("Could not copy text", err);
+
+			// reject(new Error("Could not copy text"));
+			return false;
 		}
-	});
+	} else if (oldStrToClipboard(str) !== null) {
+		// Copiado con fallback
+		return true;
+	}
+
+	return false;
 }
 
 /**
  * Share content using the share api
  * Fallsback to clipboard copy
  *
- * @export
  * @param {string} title A short title
  * @param {string} text A good description
  * @param {string} url Valid web url
  */
-export function shareThis(title: string, text: string, url: string) {
-	return new Promise<boolean>((resolve) => {
-		if ("share" in navigator) {
+export async function shareThis(title: string, text: string, url: string): Promise<boolean> {
+	if ("share" in navigator) {
+		try {
 			// share using api
-			navigator
-				.share({
-					title,
-					text,
-					url,
-				})
-				.then(() => {
-					// eslint-disable-next-line no-console
-					console.log("Successful share");
-					resolve(true);
-				})
-				.catch((error) => {
-					// eslint-disable-next-line no-console
-					console.log("Error sharing or cancelled", error);
-					resolve(false);
-				});
-		} else {
-			// copiar url al clipboard
-			resolve(srtToClipboard(url));
+			await navigator.share({ title, text, url });
+
+			// eslint-disable-next-line no-console
+			console.log("Successful share");
+
+			return true;
+		} catch (err) {
+			// eslint-disable-next-line no-console
+			console.log("Error sharing or cancelled", err);
+
+			return false;
 		}
-	});
+	} else {
+		// copiar url al clipboard
+		return srtToClipboard(url);
+	}
+
+	return false;
 }
--- scripts/stylelint.cjs
+++ scripts/stylelint.cjs
@@ -63,14 +63,16 @@ try {
 	const files = [...vueFiles, ...scssFiles];
 
 	if (files.length > 0) {
-		runStylelintInBatches(files)
-			.then(() => {
+		try {
+			runStylelintInBatches(files).then(() => {
 				const endTime = performance.now(); // End time measurement
 				const seconds = ((endTime - startTime) / 1000).toFixed(2);
 
 				console.log(`Stylelint completed in ${seconds} seconds`);
-			})
-			.catch((error) => console.error(error));
+			});
+		} catch (err) {
+			console.error(err);
+		}
 	}
 } catch (error) {
 	console.error(error.message);
