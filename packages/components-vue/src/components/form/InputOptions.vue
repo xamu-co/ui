31eb61b9c41221d76b2eadcd2b9b0af754e1e3de
Victor Saa <vis97c@hotmail.com>
2026-05-02 17:15:48 -0500

    fix(vue): prefer missing options text from meta

--- packages/components-vue/src/components/form/InputOptions.vue
+++ packages/components-vue/src/components/form/InputOptions.vue
@@ -1,6 +1,8 @@
 <template>
 	<slot v-if="!!options.length" v-bind="{ options }" :key="options.length"></slot>
-	<p v-else class="--txtColor-danger">{{ t("form_required_options") }}</p>
+	<p v-else class="--txtColor-danger">
+		{{ input.meta?.swal.missing_options || t("form_required_options") }}
+	</p>
 </template>
 
 <script setup lang="ts">
